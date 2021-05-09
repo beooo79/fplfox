0d9c2ff97767a969425ea9132b7a21f2e7409a65
beooo79 <beoo79@gmail.com>
2021-05-09 17:29:29 +0200

    init repo

---
+++ src/test/java/de/beooo79/fplfox/FplfoxApplicationTests.java
@@ -0,0 +1,13 @@
+package de.beooo79.fplfox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FplfoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
