1a8c470561382924e686236a986902305fdaf36e
beooo79 <beoo79@gmail.com>
2021-08-09 07:29:12 +0200

    more testing with parallel vs. sequential streams

--- src/main/java/de/beooo79/TestParallelStream.java
+++ src/main/java/de/beooo79/TestParallelStream.java
@@ -11,6 +11,10 @@ public class TestParallelStream {
 
     @SneakyThrows
     public static void main(String[] args) {
+        parallelVsSequential2();
+    }
+
+    private static void parallelVsSequential1() {
         System.out.println(Thread.currentThread().toString() + " started");
         StopWatch w = StopWatch.create();
 
@@ -18,7 +22,8 @@ public class TestParallelStream {
         IntStream.range(0, 200).boxed().parallel().map(i -> doWork()).forEach(TestParallelStream::printValue);
         w.stop();
         System.out.println("Parallel: " + w.toString());
-
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
         w.reset();
         w.start();
         IntStream.range(0, 200).boxed().sequential().map(i -> doWork()).forEach(TestParallelStream::printValue);
@@ -28,18 +33,49 @@ public class TestParallelStream {
         System.out.println(Thread.currentThread().toString() + " finished");
     }
 
+    private static void parallelVsSequential2() {
+        System.out.println(Thread.currentThread().toString() + " started");
+        StopWatch w = StopWatch.create();
+
+        w.start();
+        IntStream.range(0, 200).boxed().parallel().map(TestParallelStream::doEasyWork)
+                .forEachOrdered(TestParallelStream::printValue);
+        w.stop();
+        System.out.println("Parallel: " + w.toString());
+        System.out.println(
+                "--------------------------------------------------------------------------------------------");
+        w.reset();
+        w.start();
+        IntStream.range(0, 200).boxed().sequential().map(TestParallelStream::doEasyWork)
+                .forEach(TestParallelStream::printValue);
+        w.stop();
+        System.out.println("Sequential: " + w.toString());
+
+        System.out.println(Thread.currentThread().toString() + " finished");
+    }
+
     @SneakyThrows
     private static void printValue(Double value) {
-        // System.out.println(Thread.currentThread().toString() + " printIt");
-        // System.out.print(value);
+        System.out.print(value + " ");
+        // System.out.print("|");
+    }
+
+    @SneakyThrows
+    private static void printProgress(Double value) {
         System.out.print("|");
     }
 
     @SneakyThrows
     private static Double doWork() {
         // System.out.println(Thread.currentThread().toString() + " doWork");
-        Thread.sleep(100);
+        Thread.sleep(50);
         return new Random().nextDouble();
+    }
+
+    @SneakyThrows
+    private static Double doEasyWork(Integer number) {
+        Thread.sleep(50);
+        return number.doubleValue();
     }
 
 }
